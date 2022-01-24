7ece00d202f518ce2606c83343635d68072d843f
tsigab <tsigab.angosom@gmail.com>
2022-01-24 14:32:58 +0100

    MMA-5690 rettet sbd mapping

--- core/src/main/java/no/nav/dokdistdpi/consumer/dpi/dokumentpakke/sbdh/SimpleSBDSerializer.java
+++ core/src/main/java/no/nav/dokdistdpi/consumer/dpi/dokumentpakke/sbdh/SimpleSBDSerializer.java
@@ -1,6 +1,7 @@
 package no.nav.dokdistdpi.consumer.dpi.dokumentpakke.sbdh;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.SerializableString;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import no.nav.dokdistdpi.consumer.dpi.digitalpost.domain.DigitalPost;
@@ -16,16 +17,15 @@ public class SimpleSBDSerializer extends StdSerializer<SimpleStandardBusinessDoc
 	@Override
 	public void serialize(SimpleStandardBusinessDocument value, JsonGenerator gen, SerializerProvider serializerProvider) throws IOException {
 		gen.writeStartObject();
-		gen.writeFieldName("standardBusinessDocument");
-		gen.writeObject(value.getStandardBusinessDocument());
+		gen.writeObjectFieldStart("standardBusinessDocument");
 		gen.writeFieldName("standardBusinessDocumentHeader");
 		gen.writeObject(value.getStandardBusinessDocumentHeader());
 		if (value.getStandardBusinessDocument().getAny() instanceof DigitalPost) {
 			gen.writeFieldName("digital");
+			gen.writeObject(value.getStandardBusinessDocument().getAny());
 		} else {
 			throw new UnsupportedOperationException("Kun digitalmelding er støttet.");
 		}
-		gen.writeObject(value.getStandardBusinessDocument().getAny());
 		gen.writeEndObject();
 	}
 }
