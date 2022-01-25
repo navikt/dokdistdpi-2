6675e57c6b0fa779220cba19916527081db5f873
tsigab <tsigab.angosom@gmail.com>
2022-01-25 12:49:33 +0100

    MMA-5690 slettet avsenderindentifikator

--- core/src/main/java/no/nav/dokdistdpi/consumer/rdist001/DokdistAdministrerForsendelseUpdater.java
+++ core/src/main/java/no/nav/dokdistdpi/consumer/rdist001/DokdistAdministrerForsendelseUpdater.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static no.nav.dokdistdpi.utils.DokdistdpiConstant.FORSENDELSE_STATUS_OVERSENDT;
-import static no.nav.dokdistdpi.utils.DokdistdpiConstant.PROPERTY_CONVERSATION_ID;
 import static no.nav.dokdistdpi.utils.DokdistdpiConstant.PROPERTY_FORSENDELSE_ID;
 
 @Component
@@ -25,17 +24,11 @@ public class DokdistAdministrerForsendelseUpdater {
 	public void updateStatus(Exchange exchange) {
 		final String forsendelseId = exchange.getProperty(PROPERTY_FORSENDELSE_ID, String.class);
 		administrerForsendelse.oppdaterForsendelseStatus(forsendelseId, FORSENDELSE_STATUS_OVERSENDT);
-	}
-
-	public void updateStatusAndConversationId(Exchange exchange) {
-		final String forsendelseId = exchange.getProperty(PROPERTY_FORSENDELSE_ID, String.class);
-		final String conversationId = exchange.getProperty(PROPERTY_CONVERSATION_ID, String.class);
-		administrerForsendelse.oppdaterForsendelseStatusOgKonversasjonsId(forsendelseId, FORSENDELSE_STATUS_OVERSENDT, conversationId);
 		meldingTilDpiCounter();
 	}
 
 	private void meldingTilDpiCounter() {
 		meterRegistry.counter(FORSENDELSE_TIL_DPI_COUNTER,
-				FORSENDELSE_STATUS_OVERSENDT).increment();
+				"forsendelseStatus", FORSENDELSE_STATUS_OVERSENDT).increment();
 	}
 }
