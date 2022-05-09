9251da9da683a0c7b5a6b6c03829570e8c47f96b
S161464 <hakon.snotun@nav.no>
2022-05-09 09:45:56 +0200

    MMA-5789: PR kommentarer (fikse kønavn)

--- core/src/main/java/no/nav/dokdistdpi/config/jms/JmsConfig.java
+++ core/src/main/java/no/nav/dokdistdpi/config/jms/JmsConfig.java
@@ -52,8 +52,8 @@ public class JmsConfig {
 	}
 
 	@Bean
-	public Queue qdist010UtenforKjernetid(@Value("${dokdistdpi_qdist010_dist_til_dpi_kbq.queuename}") String qdist010UtenforKjernetid) throws JMSException {
-		return new MQQueue(qdist010UtenforKjernetid);
+	public Queue qdist011UtenforKjernetid(@Value("${dokdistdpi_qdist011_dist_til_dpi_kbq.queuename}") String qdist011UtenforKjernetid) throws JMSException {
+		return new MQQueue(qdist011UtenforKjernetid);
 	}
 
 	@Bean
