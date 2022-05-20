25721f3adaaed8c70a7c00ef7235730f872737b0
Joakim Borgersen <joakim.borgersen@nav.no>
2022-05-20 11:53:12 +0200

    Glemte punktum på slutten av to av tekstene

--- qdist011/src/main/java/no/nav/dokdistdpi/qdist011/Utils/VarslingstekstUtil.java
+++ qdist011/src/main/java/no/nav/dokdistdpi/qdist011/Utils/VarslingstekstUtil.java
@@ -6,8 +6,8 @@ public class VarslingstekstUtil {
 
 	//Pass på encoding her; Disse må inneholde "å".
 	public static String VEDTAK_TEKST = "Du har fått et vedtak fra NAV. Les det i din digitale postkasse.";
-	public static String VIKTIG_TEKST = "Du har fått et viktig brev fra NAV. Les det i din digitale postkasse";
-	public static String DEFAULT_TEKST = "Du har fått et brev fra NAV. Les det i din digitale postkasse";
+	public static String VIKTIG_TEKST = "Du har fått et viktig brev fra NAV. Les det i din digitale postkasse.";
+	public static String DEFAULT_TEKST = "Du har fått et brev fra NAV. Les det i din digitale postkasse.";
 
 	public static String determineVarslingstekst(DistribusjonsTypeKode distribusjonstype) {
 
