3f3b6646af6ad7bc366c20532fee9ff93d3153d1
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-09-07 07:36:40 +0200

    Siste auking i minne-mengde

--- dokdistdpi-java-opts.sh
+++ dokdistdpi-java-opts.sh
@@ -2,7 +2,7 @@
 
 JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.keyStore=${DOKDISTDPICERT_KEYSTORE}"
 JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.keyStoreType=jks"
-JAVA_OPTS="${JAVA_OPTS} -Xmx2048m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=nais"
+JAVA_OPTS="${JAVA_OPTS} -Xmx3072m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=nais"
 JAVA_OPTS="${JAVA_OPTS} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp"
 
 export JAVA_OPTS
