642dd1f6a2dff4c1001ff3d537f607a25df7d92a
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-11-29 11:39:53 +0100

    MMA-7158 Ghas november. Oppdatert avhengigheter, refaktorert og ryddet
    * Oppdatert avhengigheter
    * Fikset compile warnings
    * Fikset utdatert / deprekert config
    * Bruker resilience4j-springboot3
    * Dependabot branch bygging

--- core/src/main/java/no/nav/dokdistdpi/consumer/dpi/digitalpost/domain/SmsVarsel.java
+++ core/src/main/java/no/nav/dokdistdpi/consumer/dpi/digitalpost/domain/SmsVarsel.java
@@ -1,9 +1,11 @@
 package no.nav.dokdistdpi.consumer.dpi.digitalpost.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-@Data
+@Getter
+@Setter
 @SuperBuilder
 public class SmsVarsel extends CommonVarsel {
 	private String mobiltelefonnummer;
