776ba3aef21940dacbf8ecdd836b7a84a48082af
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-06-20 15:28:15 +0200

    Oppdaterte sertifikat for test
    * Forrige sertifikat gikk ut april 2024

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -11,25 +11,24 @@ then
     export  serviceuser_password=$(cat /secrets/serviceuser/srvdokdistdpi-2/password)
 fi
 
-export new_credentials_2023_path=/secrets/virksomhetssertifikat/sdp/credentials_2023.json
-export old_credentials_path=/secrets/virksomhetssertifikat/sdp/credentials.json
+export prod_credentials_2023_path=/secrets/virksomhetssertifikat/sdp/credentials_2023.json
+export test_credentials_2024_path=/secrets/virksomhetssertifikat/sdp/credentials_2024-03.json
 
-if test -f $new_credentials_2023_path
+if test -f $prod_credentials_2023_path
 then
     echo "Setting virksomhetssertifikat_alias"
-    export virksomhetssertifikat_alias="$(cat $new_credentials_2023_path | jq -r '.alias')"
+    export virksomhetssertifikat_alias="$(cat $prod_credentials_2023_path | jq -r '.alias')"
     echo "Setting virksomhetssertifikat_password"
-    export virksomhetssertifikat_password="$(cat $new_credentials_2023_path | jq -r '.password')"
+    export virksomhetssertifikat_password="$(cat $prod_credentials_2023_path | jq -r '.password')"
     echo "Setting virksomhetssertifikat_type"
-    export virksomhetssertifikat_type="$(cat $new_credentials_2023_path | jq -r '.type')"
+    export virksomhetssertifikat_type="$(cat $prod_credentials_2023_path | jq -r '.type')"
 else
     echo "Setting virksomhetssertifikat_alias"
-    export virksomhetssertifikat_alias="$(cat $old_credentials_path | jq -r '.alias')"
+    export virksomhetssertifikat_alias="$(cat $test_credentials_2024_path | jq -r '.alias')"
     echo "Setting virksomhetssertifikat_password"
-    export virksomhetssertifikat_password="$(cat $old_credentials_path | jq -r '.password')"
+    export virksomhetssertifikat_password="$(cat $test_credentials_2024_path | jq -r '.password')"
     echo "Setting virksomhetssertifikat_type"
-    export virksomhetssertifikat_type="$(cat $old_credentials_path | jq -r '.type')"
-
+    export virksomhetssertifikat_type="$(cat $test_credentials_2024_path | jq -r '.type')"
 fi
 
 if test -f /secrets/virksomhetssertifikat/sdp/274258896775237957919470-2023-10-11.p12.b64
@@ -38,7 +37,7 @@ then
     export virksomhetssertifikat_path="file:///secrets/virksomhetssertifikat/sdp/274258896775237957919470-2023-10-11.p12.b64"
 else
     echo "Setting virksomhetssertifikat_path"
-    export virksomhetssertifikat_path="file:///secrets/virksomhetssertifikat/sdp/key.p12.b64"
+    export virksomhetssertifikat_path="file:///secrets/virksomhetssertifikat/sdp/1956923288254923191157769-2024-03-19.p12.b64"
 fi
 
 echo "Exporting appdynamics environment variables"
